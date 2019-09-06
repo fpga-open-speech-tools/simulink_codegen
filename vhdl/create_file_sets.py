43f8b2451beb3ed43be48322f71bd84aa17471e3
Trevor Vannoy <trevorvannoy@montana.edu>
2019-09-06 10:35:16 -0600

    update fileset paths

--- vhdl/create_file_sets.py
+++ vhdl/create_file_sets.py
@@ -6,8 +6,8 @@ def create_file_sets(input_struct):
 
     # add additional dependent files before the top level file; quartus needs to synthesize dependencies first
     for i in range(len(input_struct.additional_filesets)):
-        built_string += ("add_fileset_file " + input_struct.additional_filesets[i].ljust(60, ' ') + " VHDL PATH " +
-                     "hdl/" + input_struct.additional_filesets[i] + "\n")
+        built_string += ("add_fileset_file " + input_struct.additional_filesets[i] + " VHDL PATH " +
+                      input_struct.additional_filesets[i] + "\n")
 
     # add the top level file
     built_string += "set_fileset_property QUARTUS_SYNTH TOP_LEVEL " + input_struct.quartus_synth_top_level + "\n"
@@ -15,10 +15,8 @@ def create_file_sets(input_struct):
                      str(input_struct.enable_rel_inc_paths).lower() + "\n")
     built_string += ("set_fileset_property QUARTUS_SYNTH ENABLE_FILE_OVERWRITE_MODE " +
                      str(input_struct.enable_file_overwrite).lower() + "\n")
-    built_string += ("add_fileset_file " + input_struct.vhdl_top_level_file.ljust(60) + " VHDL PATH hdl/" +
+    built_string += ("add_fileset_file " + input_struct.vhdl_top_level_file + " VHDL PATH " +
                      input_struct.vhdl_top_level_file + " TOP_LEVEL_FILE\n")
     built_string += "# end create_file_sets\n\n\n"
 
     return built_string
-
-
--- vhdl/populate_additional_filesets.py
+++ vhdl/populate_additional_filesets.py
@@ -1,7 +1,6 @@
 import os
 def populate_additional_filesets(input_struct, additionalFilesetAbsDir):
     for filename in os.listdir(additionalFilesetAbsDir):
-        if filename.endswith(".vhd") and not "axi4" in filename and not "_avalon" in filename:
-            if filename.lower() != (input_struct.model_abbreviation + "_dataplane.vhd").lower():
-                input_struct.additional_filesets.append(filename)
-                print("added file " + filename)
+        if filename.endswith(".vhd"):
+            input_struct.additional_filesets.append(filename)
+            print("added file " + filename)
