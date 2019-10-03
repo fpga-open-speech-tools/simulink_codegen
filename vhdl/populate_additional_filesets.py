5d58ed03c092bc457d5c2cb6894c18e986968431
Trevor Vannoy <vannoy.trevor@gmail.com>
2019-10-02 20:51:44 -0600

    move avalon vhdl and tcl files to hdlsrc with other hdl files

--- vhdl/populate_additional_filesets.py
+++ vhdl/populate_additional_filesets.py
@@ -3,4 +3,3 @@ def populate_additional_filesets(input_struct, additionalFilesetAbsDir):
     for filename in os.listdir(additionalFilesetAbsDir):
         if filename.endswith(".vhd") and '_avalon' not in filename:
             input_struct.additional_filesets.append(filename)
-            print("added file " + filename)
