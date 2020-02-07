994f3cd33b076e943dc7ed8ea043b701bd34a562
Trevor Vannoy <vannoy@flatearthinc.com>
2020-02-06 18:06:38 -0700

    update format of UI json
    Now everything is a list of objects

--- ui/genUiConfig.m
+++ ui/genUiConfig.m
@@ -23,26 +23,45 @@
 
 function genUiConfig(mp, outfile)
 uiConfig.module = mp.model_name;
-uiConfig.pages = containers.Map();
 
-for register = mp.register
-    registerControls = createControlObject(register);
+% create the first page and panel
+registerControls = createControlObject(mp.register(1));
+pageName = mp.register(1).uiPageName;
+panelName = mp.register(1).uiPanelName;
+panel = {struct('name', panelName, 'controls', registerControls)};
+uiConfig.pages = {struct('name', pageName, 'panels', ...
+    {panel})};
+    
 
-    page = register.ui_page;
-    panelName = register.ui_effect_name;
-    if uiConfig.pages.isKey(page)
-        % page exists, so check if the desired "effect" panel exists
-        if uiConfig.pages(page).isKey(panelName)
-            panels = uiConfig.pages(page);
-            % add register to existing effect panel
-            panels(panelName) = [panels(panelName), registerControls];
+% add the rest of the registers to the UI config
+for i=2:length(mp.register)
+    registerControls = createControlObject(mp.register(i));
+    pageName = mp.register(i).uiPageName;
+    panelName = mp.register(i).uiPanelName;
+
+    % check if the desired page already exists
+    pageIdx = findFieldValue(uiConfig.pages, 'name', pageName);
+    if pageIdx
+        % page exists, so check if the desired panel exists
+        panelIdx = findFieldValue(uiConfig.pages{pageIdx}.panels, 'name', panelName);
+        if panelIdx
+            % add register to existing panel
+            % NOTE: apparently appending to a cell array doesn't work quite like 
+            %       appending to a normal array. a = {..., ...}; a = {a, ...} DOES NOT
+            %       behave the same as a = [..., ...]; a = [a, ...]. Maybe that's not 
+            %       even a good way to append to normal arrays either... 
+            %       To append to a cell array, you need to index into a new cell
+            registerIdx = length(uiConfig.pages{pageIdx}.panels{panelIdx}.controls) + 1;
+            uiConfig.pages{pageIdx}.panels{panelIdx}.controls(registerIdx) = registerControls;
         else
-            % effect panel doesn't exist, so create one
-            panels(panelName) = registerControls;
+            % panel doesn't exist, so create one and add the register controls
+            uiConfig.pages{pageIdx}.panels = {uiConfig.pages{pageIdx}.panels, ...
+                struct('name', panelName, 'controls', registerControls)};
         end
     else
-        % page doesn't exist, so create a new page and an "effect" panel
-        uiConfig.pages(page) = containers.Map(panelName, registerControls);
+        % page doesn't exist, so create a new page and panel for the register controls
+        uiConfig.pages = {uiConfig.pages struct('name', pageName, 'panels', ...
+            struct('name', panelName, 'controls', registerControls))};
     end
 end
         
@@ -53,9 +72,19 @@ end
 function controlObject = createControlObject(register)
 controlObject.linkerName = register.widget_name;
 controlObject.type = register.widget_type;
-controlObject.range = [num2str(register.min) '-' num2str(register.max)];
+controlObject.min = register.min;
+controlObject.max = register.max;
+controlObject.dataType = register.dataType.qpointstr;
 controlObject.defaultValue = register.default;
 controlObject.units = register.widget_display_units;
 controlObject.style = register.widget_style;
 end
 
+function idx = findFieldValue(arrayOfStruct, field, value)
+idx = 0;
+for i = 1:length(arrayOfStruct)
+    if strcmpi(arrayOfStruct{i}.(field), value)
+        idx = i;
+    end
+end
+end
