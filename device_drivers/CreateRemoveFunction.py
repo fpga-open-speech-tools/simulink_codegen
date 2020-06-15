50e3cdfaa55462d628e343afdc52eb0261313fc8
Dylan Wickham <wickham@flatearthinc.com>
2020-06-15 15:41:44 -0600

    Updated driver remove function to fully remove everything device related and removed iounmap because the memory already gets freed

--- device_drivers/CreateRemoveFunction.py
+++ device_drivers/CreateRemoveFunction.py
@@ -5,10 +5,10 @@ def CreateRemoveFunction(inputParams):
     functionString += "static int " + inputParams.deviceName + "_remove(struct platform_device *pdev) {\n"
     functionString += "  fe_" + inputParams.deviceName + "_dev_t *dev = (fe_" + inputParams.deviceName + "_dev_t *)platform_get_drvdata(pdev);\n"
     functionString += "  pr_info(\"" + inputParams.deviceName + "_remove enter\\n\");\n"
+    functionString += "  device_destroy(cl, dev_num);\n"
     functionString += "  cdev_del(&dev->cdev);\n"
+    functionString += "  class_destroy(cl);\n"
     functionString += "  unregister_chrdev_region(dev_num, 2);\n"
-    if inputParams.deviceType == 2:  # FPGA device
-        functionString += "  iounmap(dev->regs);\n"
     functionString += "  pr_info(\"" + inputParams.deviceName + "_remove exit\\n\");\n"
     functionString += "  return 0;\n"
     functionString += "}\n"
