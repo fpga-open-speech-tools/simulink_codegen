fc7925ccde87dc3dae39d1d87e71b302d7697d89
Dylan Wickham <wickham@flatearthinc.com>
2020-08-06 18:45:27 -0600

    Added MIT License statement

--- device_drivers/generator.py
+++ device_drivers/generator.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+
+# @file generator.py
+#
+#     Python class to generate device drivers
+#
+#     @author Dylan Wickham
+#     @date 2020
+#     @copyright 2020 Audio Logic
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#     INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#     PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+#     FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#     Dylan Wickham
+#     Audio Logic
+#     985 Technology Blvd
+#     Bozeman, MT 59718
+#     openspeech@flatearthinc.com
+
 from .driver_config import DriverConfig
 from .device import get_device
 
